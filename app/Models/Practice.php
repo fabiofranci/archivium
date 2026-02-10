b4a50b4b401a5bfaeb082b3db31cf881067b1ba8
Fabio Franci <fabio.franci@gmail.com>
2026-02-10 21:42:49 +0100

    feat: define minimal Practice model for dynamic metadata

--- app/Models/Practice.php
+++ app/Models/Practice.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Practice extends Model
 {
-    //
+    protected $fillable = [
+        'external_code',
+        'file_path',
+    ];
+
+    /**
+     * Metadati dinamici associati alla pratica
+     */
+    public function metadata(): HasMany
+    {
+        return $this->hasMany(PracticeMetadata::class);
+    }
 }
