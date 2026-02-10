04cdcd40eb73dee853c9a0e36f572bd4bc1f227d
Fabio Franci <fabio.franci@gmail.com>
2026-02-10 22:49:44 +0100

    feat(accesso-atti): add access_request_documents relation

---
+++ database/migrations/2026_02_10_214849_create_access_request_documents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('access_request_documents', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('access_request_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('document_id')
+                ->constrained('documents')
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+
+            $table->unique(['access_request_id', 'document_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('access_request_documents');
+    }
+};
